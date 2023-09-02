ba3e7e0fc1ea8f2cf781365e865f9269ef36a9cc
GabrielRLJ0 <gabrielrlaraj@gmail.com>
2023-09-02 15:21:56 -0300

    BugFix em product_page.rb

--- Aula/Siteprismcucumber/features/page_objects/pages/product_page.rb
+++ Aula/Siteprismcucumber/features/page_objects/pages/product_page.rb
@@ -2,7 +2,7 @@ module Pages
     class ProductPage < SitePrism::Page
         set_url '/'
 
-        elements :colors
-        element  :main_image
+        elements :colors, '.productColor'
+        element  :main_image, '#mainImg'
     end
 end
